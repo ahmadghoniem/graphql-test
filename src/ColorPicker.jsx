a2b61357db49961acaf049ccf0b9b654546a8047
kontb2a22 <ahmedxd.2009@gmail.com>
2023-08-26 20:10:23 +0300

    edits

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/no-unknown-property */
 /** @jsxImportSource @emotion/react */
 
-import { css } from "@emotion/react";
-import React, { Children, cloneElement, useRef } from "react";
+import React, { Children, cloneElement } from "react";
 import chroma from "chroma-js";
 import { clamp, useCombinedRefs, useMousemove, cn } from "./utils.js";
-
+const presetCssVar = {
+  "--selected-color": "#ffffff",
+  "--selected-hue": "#ff0000",
+  "--palette-marker-x": "0",
+  "--palette-marker-y": "0",
+  "--hue-slider-y:": "0",
+};
 export const ColorPickerRoot = React.forwardRef(
   (
     { onChange, defaultValue = "#ff0000", className, children, ...props },
@@ -127,65 +132,72 @@ export const ColorPickerRoot = React.forwardRef(
       <div
         aria-label="color-picker"
         ref={picker}
+        style={presetCssVar}
         className={cn(
           "w-80 bg-[--color-picker-background] p-3 shadow-md",
           className,
         )}
-        css={css`
-          --selected-color: #ffffff;
-          --selected-hue: #ff0000;
-          --palette-marker-x: 0;
-          --palette-marker-y: 0;
-          --hue-slider-y: 0;
-        `}
         {...props}
       >
-        {Children.map(children, (child, index) =>
-          cloneElement(child, {
+        {Children.map(children, (child, index) => {
+          console.log(child);
+          return cloneElement(child, {
             ref:
-              child.type.displayName === "ColorPickerPalette"
+              child.type.displayName === "ColorPickerPanel"
                 ? palette
                 : child.type.displayName === "ColorPickerHue"
                 ? hue
                 : null,
-          }),
-        )}
+          });
+        })}
       </div>
     );
   },
 );
 ColorPickerRoot.displayName = "ColorPickerRoot";
 
-export const ColorPickerPalette = React.forwardRef(
-  ({ className, ...props }, ref) => {
+export const ColorPickerPanel = React.forwardRef(
+  ({ className, children, ...props }, ref) => {
     return (
       <div aria-label="panel" className={cn("grid h-48 gap-3", className)}>
-        <div
-          aria-label="palette"
-          className="relative bg-[--selected-hue] bg-white"
-          ref={ref}
-        >
-          <div className="absolute h-full w-full bg-[--selected-hue]">
-            <div className="absolute h-full w-full bg-gradient-to-r from-white" />
-            <div className="absolute h-full w-full bg-gradient-to-b from-transparent to-black" />
-            <div className="absolute h-full w-full bg-gradient-to-b from-transparent to-black" />
-          </div>
-          <div
-            aria-label="marker"
-            className="absolute h-4 w-4 rounded-full border-2 border-[#f7fafc] bg-[--selected-color]"
-            css={css`
-              transform: translate(
-                calc(var(--palette-marker-x, 0) * 1px - 8px),
-                calc(var(--palette-marker-y, 0) * 1px - 8px)
-              );
-            `}
-          />
+        {React.cloneElement(children, { ref })}
+      </div>
+    );
+  },
+);
+ColorPickerPanel.displayName = "ColorPickerPanel";
+
+export const ColorPickerPalette = React.forwardRef(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <div
+        aria-label="palette"
+        className={cn("relative bg-[#df3f3f] bg-[--selected-hue]", className)}
+        ref={ref}
+      >
+        <div className="absolute h-full w-full bg-[--selected-hue]">
+          <div className="absolute h-full w-full bg-gradient-to-r from-white" />
+          <div className="absolute h-full w-full bg-gradient-to-b from-transparent to-black" />
         </div>
+
+        {children}
       </div>
     );
   },
 );
 ColorPickerPalette.displayName = "ColorPickerPalette";
+// bg could be transparent as well ! bg-transparent
+export const ColorPickerMarker = React.forwardRef(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <div
+        aria-label="marker"
+        className="absolute h-4 w-4 translate-x-[calc(var(--palette-marker-x,0)*1px-8px)] translate-y-[calc(var(--palette-marker-y,0)*1px-8px)] transform rounded-full border-2 border-[#f7fafc] bg-[--selected-color]"
+      />
+    );
+  },
+);
+ColorPickerMarker.displayName = "ColorPickerMarker";
 
 export const ColorPickerHue = React.forwardRef(
   ({ className, ...props }, ref) => {
@@ -198,29 +210,10 @@ export const ColorPickerHue = React.forwardRef(
         <div className="absolute h-full w-full bg-[image:--hue-gradient]" />
         <div
           aria-label="slider"
-          className="absolute -left-[0.0125rem] h-[0.625rem] rounded-full border-2 border-[#f7fafc] bg-[--selected-hue]"
-          css={css`
-            width: calc(100% + 4px);
-            transform: translate(0px, calc(var(--hue-slider-y, 0) * 1px - 5px));
-          `}
+          className="absolute -left-[0.0125rem] h-[0.625rem] w-[calc(100%+0.25rem)] translate-y-[calc(var(--hue-slider-y,0)*1px-5px)] rounded-full border-2 border-[#f7fafc] bg-[--selected-hue]"
         />
       </div>
     );
   },
 );
 ColorPickerHue.displayName = "ColorPickerHue";
-
-export const ColorPickerResult = React.forwardRef(
-  ({ className, children, ...props }, ref) => {
-    return (
-      <div
-        aria-label="result"
-        ref={ref}
-        className={cn("relative h-12 text-[--result-text-color]", className)}
-      >
-        {children}
-      </div>
-    );
-  },
-);
-ColorPickerResult.displayName = "ColorPickerResult";
--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -3,10 +3,15 @@ import {
   ColorPickerRoot,
   ColorPickerPalette,
   ColorPickerHue,
+  ColorPickerPanel,
+  ColorPickerMarker,
 } from "./ColorPicker";
 // use text-[--result-text-color] to get the inverted text color on a color-changing background
 // use value.rgb() to get the selected color in rgb format
 // use value.css() to get the selected color in css usable format
+// Usestate for text-color with it's handle change
+// accessibility  and shadncn collab
+// use native for zero dependencies rather than chroma-js
 export const MyApp = () => {
   const [value, setValue] = useState(null);
   console.log(value?.rgb());
@@ -19,7 +24,11 @@ export const MyApp = () => {
       <p className="col-span-12 row-span-1 bg-[--selected-color] text-[--result-text-color]">
         hello world
       </p>
-      <ColorPickerPalette className="col-span-10 row-span-3 w-full" />
+      <ColorPickerPanel className="col-span-10 row-span-3 w-full">
+        <ColorPickerPalette>
+          <ColorPickerMarker />
+        </ColorPickerPalette>
+      </ColorPickerPanel>
       <ColorPickerHue className="col-span-2 row-span-3 h-full w-2" />
     </ColorPickerRoot>
   );
